4c79ff20a91699ddfde177b6a4d83f2c684b14a7
MustafaMadkour <madkour.dev@gmail.com>
2020-08-31 19:12:59 +0200

    create a class based component and nest it in the root component

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Mad from './Mad';
 
 function App() {
   return (
     <div className="App">
       <h1>My first React app</h1>
       <p>Welcome :)</p>
+      <Mad />
     </div>
   );
 }
---
+++ myapp/src/Mad.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Mad extends Component {
+    render() {
+        return(
+            <div className="mad">
+                <div>Name: Mustafa</div>
+                <div>Age: 30</div>
+                <div>City: Alexandria</div>
+            </div>
+        )
+    }
+}
+
+export default Mad;
