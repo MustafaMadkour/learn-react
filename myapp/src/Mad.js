85726ecca2686d8e1ec4262886118c405e2dc42f
MustafaMadkour <madkour.dev@gmail.com>
2020-09-01 17:17:49 +0200

    statless components(functional)(ui-components)

--- myapp/src/Mad.js
+++ myapp/src/Mad.js
@@ -1,24 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Mad extends Component {
-    render() {
-
-        const { mads } = this.props;
-        const madList = mads.map(mad => {
-            return(
-                <div className="mad" key={mad.id}>
-                    <div>Name: {mad.name}</div>
-                    <div>Age: {mad.age}</div>
-                    <div>City: {mad.city}</div>
-                </div>
-            )
-        });
+const Mad = ({ mads }) => {
+    const madList = mads.map(mad => {
         return(
-            <div className="mad-list">
-                { madList }
+            <div className="mad" key={mad.id}>
+                <div>Name: {mad.name}</div>
+                <div>Age: {mad.age}</div>
+                <div>City: {mad.city}</div>
             </div>
         )
-    }
+    });
+    return(
+        <div className="mad-list">
+            { madList }
+        </div>
+    )
 }
 
 export default Mad;
